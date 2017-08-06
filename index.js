4aa8403a9654ee0493e23546feef0c4825fdab35
Hoang Anh Vo <Hoang Anh Vo>
2017-08-06 14:53:16 +0300

    Create store

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Scoreboard from './src/containers/Scoreboard';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import PlayerReducer from './src/reducers/player';
+
+const store = createStore(
+  PlayerReducer
+);
 
 render(
-  <Scoreboard />, document.getElementById('root')
+  <Provider store = {store}>
+    <Scoreboard />
+  </Provider>,
+  document.getElementById('root')
 )
